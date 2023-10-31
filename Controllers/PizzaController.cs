1fffce01780014c60b86ba5b7bae8d9d7c2d82bf
Zack Spring Chief <jzacksc@gmail.com>
2023-10-31 16:55:01 -0600

    controllers is spelt with a 'r'

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ContosoPizza.Controllers;
 
 [ApiController]
-[Route("[contoller]")]
+[Route("[controller]")]
 public class PizzaController : ControllerBase
 {
     public PizzaController()
