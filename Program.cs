7a07b2001f99fdc6410695701dfe6be7361fb68c
Zack Spring Chief <jzacksc@gmail.com>
2023-11-01 23:09:23 -0600

    adding loop

--- Program.cs
+++ Program.cs
@@ -24,3 +24,30 @@ app.MapControllers();
 
 app.Run();
 //need to see what i can add for mongo
+
+// namespace SoloLearn
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             int[] numbers = new int[5];
+//             int count = 0;
+//             int sum = 0;
+//             while (count <5)
+//             {
+//                 numbers[count] = Convert.ToInt32(Console.ReadLine());
+//                 count++;
+//             }
+//             //your code goes here
+//             for (int i = 0; i < numbers.Length; i++)
+//             {
+//                 if(numbers[i]%2 == 0)
+//                 {
+//                     sum += numbers[i];
+//                 }
+//             }
+//             Console.WriteLine(sum);
+//         }
+//     }
+// }
