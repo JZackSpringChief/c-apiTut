72fd607aac456b7cd8022d73fdd585fa5dfbefec
Zack Spring Chief <jzacksc@gmail.com>
2023-11-08 12:49:02 -0700

    adding .. Calories to dataset

--- Models/Drinks.cs
+++ Models/Drinks.cs
@@ -4,6 +4,7 @@ public class Drinks
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public bool IsGlutenFree { get; set; }
+    public bool SugarFree { get; set; }
+    public int Calories { get; set; }
 }
 //add another model
